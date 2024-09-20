1cff896ec699ead843a3cc8baf4e1661fee6272c
ParanoidAndroid <shaayaanahmed@gmail.com>
2024-09-20 20:28:54 +0600

    bruhh

--- ExoFbay/login.php
+++ ExoFbay/login.php
@@ -12,20 +12,33 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $sql = "SELECT u.user_id as user_id, u.email, u.password, c.customer_id  FROM user u, customer c WHERE email = '$email' AND password = '$password' and c.user_id = u.user_id";
-    $result = $conn->query(query: $sql);
-    $row = mysqli_fetch_assoc($result);
-    if ($result->num_rows > 0) {
-        //echo "login successfull";
-        // $_SESSION["user_name"] = $conn->query(query: "SELECT name FROM user WHERE email = '$email' AND password = '$password'");
-        $_SESSION["user_id"] = $row['user_id'];
-        $_SESSION["customer_id"] = $row['customer_id'];
-        $_SESSION["email"] = $email;
-        header("Location: index.html");
-        $conn->close();
-    } else {
-        header("Location: login_signup.html");
-        $conn->close();
+    $flagsql = "SELECT flag from user";
+    $flagresult = $conn->query($flagsql);
+    $flagrow = mysqli_fetch_assoc($result);
+    $flag = $flagrow["flag"];
+    if ($flag == 1){
+        $sql = "SELECT u.user_id as user_id, u.email, u.password, c.customer_id, FROM user u, customer c WHERE email = '$email' AND password = '$password' and c.user_id = u.user_id";
+        $result = $conn->query(query: $sql);
+        $row = mysqli_fetch_assoc($result);
+        if ($result->num_rows > 0) {
+            $_SESSION["user_id"] = $row['user_id'];
+            $_SESSION["customer_id"] = $row['customer_id'];
+            $_SESSION["email"] = $email;
+            header("Location: index.html");
+            $conn->close();
+        } 
+    }   else if ($flag == 2){
+        $sql = "SELECT u.user_id as user_id, u.email, u.password, s.supplier_id, FROM user u, supplier s WHERE email = '$email' AND password = '$password' and s.user_id = u.user_id";
+        $result = $conn->query(query: $sql);
+        $row = mysqli_fetch_assoc($result);
+        if ($result->num_rows > 0) {
+            $_SESSION["user_id"] = $row['user_id'];
+            $_SESSION["supplier_id"] = $row['supplier_id'];
+            $_SESSION["email"] = $email;
+            header("Location: supplier.html");
+            $conn->close()
+}
     }
+
 }
 ?>
--- ExoFbay/signup.php
+++ ExoFbay/signup.php
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $city = $_POST['city'];
     $Supplier = $_POST['supplier'];
     if($Supplier == "Syes"){
-        $flag = 0;
+        $flag = 2;
         $coo = $_POST['origin'];
         
     }   else{
@@ -33,18 +33,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
     }
     
-    $sql1 = "INSERT INTO user (user_id, email,name, password, flag, building, road, area, city) VALUES ('$user_id','$username', '$userpassword', '$flag','$building','$road','$area','$city')";
+    $sql1 = "INSERT INTO user (user_id, email, name, password, flag, building, road, area, city) VALUES ('$user_id','$email','$username', '$userpassword', '$flag','$building','$road','$area','$city')";
 
 
-    if ($conn->query(query: $sql) === TRUE) {
+    if ($conn->query(query: $sql1) === TRUE) {
         $_SESSION["user_id"] = $row['user_id'];
         $_SESSION["email"] = $email;
-        if($flag == 0){
+        if($flag == 2){
             $sql = "INSERT INTO supplier (supplier_id, country_of_origin, user_id) 
-            VALUES ('$coo','$user_id')";
-
+            VALUES (NULL, '$coo','$user_id')";
+            $conn->query(query: $sql);
+            header("Location: supplier.html");
+        }  if($flag == 1){
+            $sql = "INSERT INTO customer(customer_id, user_id) 
+            VALUES (NULL,'$user_id')";
+            $conn->query(query: $sql);
+            header("Location: index.html");
         }
-        header("Location: index.html");
+
+
     } else {
         echo "Error: " . $conn->error;
     }
